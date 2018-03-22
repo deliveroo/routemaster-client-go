b92bcb453d3b585c2296bd7c4b90c48b58dc8ecc
Jeffrey Lo <jeff.lo@deliveroo.com>
2018-03-22 16:03:02 -0400

    Add OnError hook to listener
    Allow callers to supply a callback when errors are emitted from this
    package.

--- listener.go
+++ listener.go
@@ -2,77 +2,121 @@ package routemaster
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/deliveroo/routemaster-client-go/pkg/glog"
-	"github.com/deliveroo/routemaster-client-go/pkg/logmsg"
+	"os"
 )
 
 // The HandlerFunc type represents a function signature for consuming events
 // received from Routemaster.
-type HandlerFunc func([]*ReceivedEvent)
+type HandlerFunc func([]*ReceivedEvent) error
+
+// The onError represents a function signature for handling
+// errors emitted by the listener.
+type onError func(error)
+
+// ListenerConfig specifies the way a listener should be set up.
+type ListenerConfig struct {
+	Handler HandlerFunc
+	Logger  *log.Logger
+	OnError onError
+	UUID    string
+}
 
 // A Listener is an implementation of http.Handler that handles Routemaster
 // events.
 type Listener struct {
-	uuid    string
 	handler HandlerFunc
 	logger  *log.Logger
+	onError onError
+	uuid    string
 }
 
 // NewListener creates a new handler for receiving Routemaster events.
-func NewListener(uuid string, handler HandlerFunc, logger *log.Logger) *Listener {
+func NewListener(cfg *ListenerConfig) *Listener {
 	return &Listener{
-		uuid:    uuid,
-		handler: handler,
-		logger:  glog.DefaultLogger(logger),
+		handler: cfg.Handler,
+		logger:  defaultLogger(cfg.Logger),
+		onError: cfg.OnError,
+		uuid:    cfg.UUID,
 	}
 }
 
-// error replies to the request with the specified HTTP code, and a simple
+// reportError replies to the request with the specified HTTP code, and a simple
 // message based on the code.
-func (l *Listener) error(w http.ResponseWriter, code int) {
-	text := fmt.Sprintf("%d %s", code, http.StatusText(code))
-	http.Error(w, text, code)
-}
+func (l *Listener) reportError(w http.ResponseWriter, code int, err error) {
+	var wroteError bool
 
-// handleEvents runs the handler, catching any panics and returning a
-// 500.
-func (l *Listener) handleEvents(w http.ResponseWriter, events []*ReceivedEvent) {
+	// If onError panics, log the panic and continue to send the
+	// intended status code back.
 	defer func() {
-		if err := recover(); err != nil {
-			l.logger.Print(logmsg.Error("routemaster: panic during event handler").Set("error", err))
-			l.error(w, http.StatusInternalServerError)
+		if r := recover(); r != nil {
+			l.logger.Printf("panic while running error handler: %+v", r)
+			if !wroteError {
+				text := fmt.Sprintf("%d %s", code, http.StatusText(code))
+				http.Error(w, text, code)
+			}
 		}
 	}()
-	l.handler(events)
+
+	if l.onError != nil {
+		l.onError(err)
+	}
+
+	text := fmt.Sprintf("%d %s", code, http.StatusText(code))
+	http.Error(w, text, code)
+	wroteError = true
 }
 
 func (l *Listener) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			err, ok := r.(error)
+			if !ok {
+				err = fmt.Errorf("%+v", r)
+			}
+			l.reportError(w, http.StatusInternalServerError, err)
+			return
+		}
+	}()
+
+	// Check for the expected username.
 	username, _, _ := r.BasicAuth()
 	if username != l.uuid {
-		l.error(w, http.StatusUnauthorized)
-		l.logger.Print(logmsg.Error("routemaster: unauthorized"))
+		l.reportError(w, http.StatusUnauthorized, errors.New("bad token"))
 		return
 	}
 
+	// Read the request body.
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
-		l.error(w, http.StatusInternalServerError)
-		l.logger.Print(logmsg.Error("routemaster: error reading body").SetError(err))
+		l.reportError(w, http.StatusInternalServerError, errors.New("request body read failed"))
 		return
 	}
 
+	// Unmarshal received events.
 	var events []*ReceivedEvent
 	err = json.Unmarshal(b, &events)
 	if err != nil || len(events) == 0 {
-		l.logger.Print(logmsg.Error("routemaster: bad request").Set("event", string(b)))
-		l.error(w, http.StatusBadRequest)
+		l.reportError(w, http.StatusBadRequest, fmt.Errorf("body malformed: %s", string(b)))
 		return
 	}
-	l.handleEvents(w, events)
+
+	// Finally, handle events.
+	if err := l.handler(events); err != nil {
+		l.reportError(w, http.StatusInternalServerError, err)
+		return
+	}
+}
+
+// defaultLogger returns a logger if the given one is nil.
+func defaultLogger(logger *log.Logger) *log.Logger {
+	if logger != nil {
+		return logger
+	}
+	return log.New(os.Stderr, "routemaster/client: ", log.LstdFlags)
 }
